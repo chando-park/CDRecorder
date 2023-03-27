e92f02dd9cd0bc62ecfe534e55efa238770a4acf
박찬도 <montelia@naver.com>
2023-03-27 15:55:11 +0900

    init

--- Sources/CDRecorder/CDRecorder.swift
+++ Sources/CDRecorder/CDRecorder.swift
@@ -1,6 +1,103 @@
-public struct CDRecorder {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import AVKit
 
-    public init() {
+public class CDRecorder: NSObject{
+    
+    public enum Status{
+        case start(success: Bool, errorMessage: String?)
+        case end(success: Bool, errorMessage: String?)
+    }
+    
+    public typealias RecordingProcessBlock = (_ status: Status) -> ()
+    public typealias RecordPrepareCompletedBlock = (_ isSuccess: Bool, _ errorMessage: String?) -> ()
+
+    private var _audioRecorder: AVAudioRecorder?
+    private var _recordSession: AVAudioSession!
+    
+    private let _audioSetting: [String : Any]
+    private let _microphoneNotUsableMessage: String?
+    private let _recordingProcessCompltedBlock: RecordingProcessBlock
+    
+    public init(audioSetting: [String : Any] =
+                [
+                    AVFormatIDKey: kAudioFormatMPEG4AAC,
+                    AVSampleRateKey: 48000,
+                    AVNumberOfChannelsKey: 1,
+                    AVEncoderBitRateKey: 64000,
+                    AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+                ],
+                microphoneNotUsableMessage: String? = nil,
+                completdBolck: @escaping RecordingProcessBlock) {
+        self._audioSetting = audioSetting
+        self._microphoneNotUsableMessage = microphoneNotUsableMessage
+        self._recordingProcessCompltedBlock = completdBolck
+        
+        super.init()
+    }
+    
+    public var isRecording: Bool {
+        if let _ = self._audioRecorder{
+            return true
+        }
+        return false
+    }
+
+    public func prepare(complted: @escaping RecordPrepareCompletedBlock) {
+        do {
+            self._recordSession = AVAudioSession.sharedInstance()
+            self._recordSession.requestRecordPermission() {[unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        do{
+
+                            try self._recordSession.setCategory(AVAudioSession.Category.playAndRecord,
+                                                               mode: AVAudioSession.Mode.default,
+                                                               options: [.mixWithOthers,.allowBluetooth,.defaultToSpeaker])
+                            try self._recordSession.setActive(true)
+                            
+                            complted(true, nil)
+                        }catch let e{
+                            complted(false, e.localizedDescription)
+                        }
+                    } else {
+                        complted(false, self._microphoneNotUsableMessage ?? "마이크 권한을 허용해 주세요.")
+                    }
+                }
+            }
+        }
+    }
+    
+    public func startRecording(fileUrl: URL, duration: Double){
+        do{
+            self._audioRecorder = try AVAudioRecorder(url: fileUrl, settings: self._audioSetting)
+            self._audioRecorder?.delegate = self
+            self._audioRecorder?.record(forDuration: duration)
+            
+            self._recordingProcessCompltedBlock(.start(success: true, errorMessage: nil))
+        }catch let e{
+            self.finishRecording()
+            
+            self._recordingProcessCompltedBlock(.start(success: false, errorMessage: e.localizedDescription))
+            
+        }
+    }
+
+    public func finishRecording(){
+        self._audioRecorder?.stop()
+        self._audioRecorder = nil
+        
+        try? self._recordSession?.setActive(false)
+    }
+
+}
+
+extension CDRecorder: AVAudioRecorderDelegate{
+    public func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        self._recordingProcessCompltedBlock(.end(success: true, errorMessage: nil))
+    }
+    
+    public func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        self._recordingProcessCompltedBlock(.end(success: true, errorMessage: error?.localizedDescription))
     }
 }
+
--- Tests/CDRecorderTests/CDRecorderTests.swift
+++ Tests/CDRecorderTests/CDRecorderTests.swift
@@ -2,10 +2,38 @@ import XCTest
 @testable import CDRecorder
 
 final class CDRecorderTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CDRecorder().text, "Hello, World!")
+    
+    var recorder: CDRecorder?
+    
+    override func setUpWithError() throws {
+        
+    }
+    
+    override func tearDownWithError() throws {
+        self.recorder?.finishRecording()
+        self.recorder = nil
+    }
+    
+    func testPrepareRecord() throws {
+//        let exStart = self.expectation(description: "exStart")
+        
+        self.recorder = CDRecorder(completdBolck: { status in
+            
+            switch status {
+            case .start(let success, let errorMessage):
+                break
+            case .end(let success, let errorMessage):
+                break
+            }
+        })
+        
+        
+        let ex = self.expectation(description: "testPrepareRecord")
+        self.recorder?.prepare(complted: { isSuccess, errorMessage in
+            ex.fulfill()
+            XCTAssertTrue(isSuccess, errorMessage ?? "")
+        })
+        
+        wait(for: [ex], timeout: 4)
     }
 }
